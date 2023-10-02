f8afc09a2bcc0468f90278292d2d5daeaea122cb
Vaneet Gupta <vaneet.gupta47@gmail.com>
2023-10-02 21:59:19 +0530

    infinity scroll logic

--- script.js
+++ script.js
@@ -1,32 +1,53 @@
 const imageContainer = document.getElementById('image-container');
 const loader = document.getElementById('loader');
 
+let ready = false;
+let imagesLoaded = 0;
+let totalImages = 0;
 let photosArray = [];
 
-const count = 10;
+const count = 30;
 const apiKey = 'z0o1skzmd2urRfc5KUWS0WiuCZ4Go2tCrlh27dmHrkU';
 
 const apiUrl = `https://api.unsplash.com/photos/random?client_id=${apiKey}&count=${count}`;
 
-// Create Elements For Links and Photos, Add to DOM
+function setAttributes(element, attributes) {
+  for (const key in attributes) {
+    element.setAttribute(key, attributes[key]);
+  }
+}
+
+function imageLoaded() {
+  imagesLoaded++;
+  if (imagesLoaded === totalImages) {
+    ready = true;
+    loader.hidden = true;
+  }
+}
+
 function displayPhotos() {
+  imagesLoaded = 0;
+  totalImages = photosArray.length;
   photosArray.forEach((photo) => {
     const item = document.createElement('a');
-    item.setAttribute('href', photo.links.html);
-    item.setAttribute('target', '_blank');
-
+    setAttributes(item, {
+      href: photo.links.html,
+      target: '_blank',
+    });
     const img = document.createElement('img');
-    img.setAttribute('src', photo.urls.regular);
-    img.setAttribute('alt', photo.alt_description);
-    img.setAttribute('title', photo.alt_description);
+    setAttributes(img, {
+      src: photo.urls.regular,
+      alt: photo.alt_description,
+      title: photo.alt_description,
+    });
 
+    img.addEventListener('load', imageLoaded);
     item.appendChild(img);
 
     imageContainer.appendChild(item);
   });
 }
 
-// get photos from unsplash api
 async function getPhotos() {
   try {
     const response = await fetch(apiUrl);
@@ -34,5 +55,20 @@ async function getPhotos() {
     displayPhotos();
   } catch (error) {}
 }
+
+// window.innerHeight -> Total height of browser window
+// window.scrollY -> Distance from top of page user has scrolled
+// document.body.offsetHeight -> Height of everything in the body, including what is not within the view
+
+// Check to see if scrolling near the bottom of the page, load more photos
+window.addEventListener('scroll', () => {
+  if (
+    window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 &&
+    ready
+  ) {
+    ready = false;
+    getPhotos();
+  }
+});
 
 getPhotos();
